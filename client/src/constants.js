749071e95928f019e1d4b811fd3a8ee384329228
Tobi-Daniel <oloyebmv@gmail.com>
2024-05-17 01:14:40 +0100

    testing production

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:7000";
-export const BACKEND_URL = "http://localhost:7000";
+export const BASE_URL = "https://luxury-dealr.onrender.com";
+export const BACKEND_URL = "https://luxury-dealr.onrender.com";
 export const CATEGORIES_URL = "api/categories";
 export const PRODUCTS_URL = "api/products";
 export const ORDERS_URL = "api/orders";
